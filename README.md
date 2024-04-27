a754ed7883bdac8efa895996d51716eb9d645993
Daniel Steven Campaz Ocoro <86497840+DanielCampaz@users.noreply.github.com>
2024-04-27 16:40:33 -0500

    Add Supertest and exclude tests files

---
+++ README.md
@@ -0,0 +1,11 @@
+![Logo](https://miro.medium.com/v2/resize:fit:900/1*rVjj_JOGVJIS8pGEJVrLww.png)
+
+# Muckup: Node-js Typescript
+
+Template for Node js API with typescript, with eslint configured, Jest for unit tests and supertes for the API, this code is adjusted to the default configuration of all my projects using this stock.
+
+Developed by [Daniel Campaz](https://github.com/DanielCampaz), I hope it helps you
+
+# Enviroments:
+
+Change .enviroment for .env or .env.local
--- src/main.test.ts
+++ src/main.test.ts
@@ -4,3 +4,17 @@ test("Main Functions", () => {
   expect(main()).toBe("Example");
   // expect(main()).toBe(2);
 });
+
+/**
+ * 
+ * Whit SuperTest
+ * describe('Audio Router Test /audio/**', () => {
+  describe('GET /audio', () => {
+    test('should response whit a 200 status code', async () => {
+                         <--- Import of supertest --->
+      const response = await request(app).get('/audio').send();
+      expect(response.statusCode).toBe(200);
+    });
+  });
+});
+ */
